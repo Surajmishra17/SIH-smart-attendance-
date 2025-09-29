5299f779fdb28909a5fa5919bdaceddc565ccdef
GreyFox00001 <reachtanish@gmail.com>
2025-09-29 22:23:06 +0530

    dbstring correction

--- db/user.js
+++ db/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // This is the connection string for your MongoDB database.
 // 'attenSysDb' will be the name of the database created in MongoDB.
-const dbURI = 'mongodb+srv://reachtanish:Tanish123@cluster0.0jq9vh4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const dbURI = 'mongodb+srv://reachtanish:<dbpass>@cluster0.0jq9vh4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const connectDB = async () => {
     try {
